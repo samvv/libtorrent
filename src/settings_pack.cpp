1d79598b495dd234993ec3c02ceecf9c4a3493a3
Sam Vervaeck <samvv@pm.me>
2026-05-14 21:04:45 +0200

    Fix NAT-PMP gateway update not applying to current session

--- src/settings_pack.cpp
+++ src/settings_pack.cpp
@@ -150,7 +150,7 @@ constexpr int DISK_WRITE_MODE = settings_pack::enable_os_cache;
 		SET(i2p_hostname, "", &session_impl::update_i2p_bridge),
 		SET(peer_fingerprint, "-LT20C0-", nullptr),
 		SET(dht_bootstrap_nodes, "dht.libtorrent.org:25401", &session_impl::update_dht_bootstrap_nodes),
-		SET(natpmp_gateway, "", nullptr)
+		SET(natpmp_gateway, "", &session_impl::update_natpmp)
 	}});
 
 	CONSTEXPR_SETTINGS
